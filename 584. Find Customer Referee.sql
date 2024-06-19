5e1fa0b3a994cb885fb7e813d54992faef81cc66
Gamal Ahmed <75225936+gamalahmed3265@users.noreply.github.com>
2024-06-19 18:00:35 +0300

    584. Find Customer Referee

--- 584. Find Customer Referee.sql
+++ 584. Find Customer Referee.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select name from Customer where referee_id !=2 or referee_id is null;
+select name,population,area from World where area>=3000000 or population >=25000000;
